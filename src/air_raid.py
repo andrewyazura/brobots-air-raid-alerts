9e5342c4b58ab3d129e30a952d973aec592deaff
Andrew Yatsura <andrewyazura203@gmail.com>
2022-12-01 04:14:24 +0000

    Use another API to get air raid alert status

--- src/air_raid.py
+++ src/air_raid.py
@@ -2,7 +2,6 @@ from logging import getLogger
 from urllib.parse import urljoin
 
 import requests
-from bs4 import BeautifulSoup
 
 from src import current_bot
 
@@ -15,25 +14,17 @@ class AirRaidApi:
 
     def get_status(self, tag: str) -> bool:
         response = requests.get(
-            url=self.make_url("/s/air_alert_ua"),
-            timeout=self.timeout,
-            params={"q": str(tag)},
+            url=self.url, timeout=self.timeout
         )
 
         if not response.text:
             self.logger.debug("response is empty")
 
-        soup = BeautifulSoup(markup=response.text, features="html.parser")
-        last_message = soup.find_all(
-            name="div", attrs={"class": "tgme_widget_message"}
-        )[-1]
-        message_text = last_message.find(
-            name="div", attrs={"class": "tgme_widget_message_text"}
-        ).getText()
+        for alert in response["alerts"]:
+            if tag in alert["location_title"]:
+                return True
 
-        air_raid = "відбій" not in message_text.lower()
-
-        return air_raid
+        return False
 
     def make_url(self, endpoint: str) -> str:
         return urljoin(self.url, endpoint)
